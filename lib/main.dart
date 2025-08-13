567404c0748681559d94fe15810c30af00a0a73c
Abdalaziz <abdalaziztll992010@gmail.com>
2025-08-13 19:02:01 +0300

    init point

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,42 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+List<Widget> pages = [ScorePage(), Scaffold(), Scaffold()];
+int indexOfPage = 0;
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: ScorePage());
+    return MaterialApp(
+      home: Scaffold(
+        body: pages[indexOfPage],
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: indexOfPage,
+          onDestinationSelected: (value) {
+            indexOfPage = value;
+            setState(() {});
+          },
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+
+            NavigationDestination(
+              selectedIcon: Icon(Icons.facebook),
+              tooltip: "FaceBook",
+              icon: Icon(Icons.person),
+              label: "Profile",
+            ),
+            NavigationDestination(icon: Icon(Icons.set_meal), label: "Meals"),
+          ],
+        ),
+      ),
+    );
   }
 }
 
@@ -36,172 +66,199 @@ class ScorePage extends StatelessWidget {
                 color: Colors.purple,
               ),
             ),
-            Column(
-              children: [
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 40, top: 100),
-                      child: IconButton(
-                        onPressed: () {},
-                        icon: Icon(Icons.arrow_back),
-                      ),
-                    ),
-                  ],
-                ),
-                Center(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Color(0x50FFFFFF),
-                    ),
-                    width: 200,
-                    height: 200,
-                    child: Container(
-                      margin: EdgeInsets.all(16),
-                      padding: EdgeInsets.all(16),
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Color(0x90FFFFFF),
-                      ),
-                      alignment: Alignment.center,
-                      child: Container(
-                        alignment: Alignment.center,
-                        width: 120,
-                        height: 120,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Color(0xFFFFFFFF),
-                        ),
-                        child: Text("Your Score Is"),
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.all(40),
-                  width: 305,
-                  height: 160,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    gradient: LinearGradient(
-                      colors: [Colors.pink, Colors.yellow],
-                    ),
-                  ),
-                  child: GridView(
-                    physics: NeverScrollableScrollPhysics(),
-                    padding: EdgeInsets.all(20),
-                    // shrinkWrap: true,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      mainAxisExtent: 60
-                    ),
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  Row(
                     children: [
-                      Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            "* 100%",
-                            style: TextStyle(color: Colors.purple),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 40),
-                            child: Text("Completion"),
-                          ),
-                        ],
-                      ),Column(
-
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            "* 100%",
-                            style: TextStyle(color: Colors.purple),
-                          ),
-                          Text("Completion"),
-                        ],
-                      ),Column(
-
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            "* 100%",
-                            style: TextStyle(color: Colors.purple),
-                          ),
-                          Text("Completion"),
-                        ],
-                      ),Column(
-
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            "* 100%",
-                            style: TextStyle(color: Colors.purple),
-                          ),
-                          Text("Completion"),
-                        ],
+                      Padding(
+                        padding: const EdgeInsets.only(left: 40, top: 100),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.arrow_back),
+                        ),
                       ),
                     ],
                   ),
-                ),
-                Container(
-                  height: 300,
-                  child: GridView(
-                    physics: NeverScrollableScrollPhysics(),
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-                  children: [
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
+                  Center(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color(0x50FFFFFF),
+                      ),
+                      width: 200,
+                      height: 200,
+                      child: Container(
+                        margin: EdgeInsets.all(16),
+                        padding: EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Color(0x90FFFFFF),
+                        ),
+                        alignment: Alignment.center,
+                        child: Container(
+                          alignment: Alignment.center,
+                          width: 120,
+                          height: 120,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(0xFFFFFFFF),
+                          ),
+                          child: Text("Your Score Is"),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(40),
+                    width: 305,
+                    height: 160,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      gradient: LinearGradient(
+                        colors: [Colors.pink, Colors.yellow],
+                      ),
+                    ),
+                    child: GridView(
+                      physics: NeverScrollableScrollPhysics(),
+                      padding: EdgeInsets.all(20),
+                      // shrinkWrap: true,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        mainAxisExtent: 60,
+                      ),
                       children: [
-                        IconButton(onPressed: (){}, icon: Icon(Icons.share)),
-                        Text("Share Again")
-                      ],
-                    ),Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        IconButton(onPressed: (){
-                          SharePlus.instance.share(ShareParams(
-                            text: 
-                        """
-                        I Install Quiz Flutter App , it is very interteing app
-                        And I Get a Score About {Score}
-
-
-                        please install the app and try it with me 
-                        """
-                  
-                          ));
-                        }, icon: Icon(Icons.share)),
-                        Text("Share Again")
-                      ],
-                    ),Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        IconButton(onPressed: (){}, icon: Icon(Icons.share)),
-                        Text("Share Again")
-                      ],
-                    ),Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        IconButton(onPressed: (){}, icon: Icon(Icons.share)),
-                        Text("Share Again")
-                      ],
-                    ),Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        IconButton(onPressed: (){}, icon: Icon(Icons.share)),
-                        Text("Share Again")
-                      ],
-                    ),Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        IconButton(onPressed: (){}, icon: Icon(Icons.share)),
-                        Text("Share Again")
+                        Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              "* 100%",
+                              style: TextStyle(color: Colors.purple),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 40),
+                              child: Text("Completion"),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              "* 100%",
+                              style: TextStyle(color: Colors.purple),
+                            ),
+                            Text("Completion"),
+                          ],
+                        ),
+                        Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              "* 100%",
+                              style: TextStyle(color: Colors.purple),
+                            ),
+                            Text("Completion"),
+                          ],
+                        ),
+                        Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              "* 100%",
+                              style: TextStyle(color: Colors.purple),
+                            ),
+                            Text("Completion"),
+                          ],
+                        ),
                       ],
                     ),
-                  ],
                   ),
-                )
-              ],
+                  Container(
+                    height: 300,
+                    child: GridView(
+                      physics: NeverScrollableScrollPhysics(),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                      ),
+                      children: [
+                        Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.share),
+                            ),
+                            Text("Share Again"),
+                          ],
+                        ),
+                        Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              onPressed: () {
+                                SharePlus.instance.share(
+                                  ShareParams(
+                                    text: """
+                          I Install Quiz Flutter App , it is very interteing app
+                          And I Get a Score About {Score}
+              
+              
+                          please install the app and try it with me 
+                          """,
+                                  ),
+                                );
+                              },
+                              icon: Icon(Icons.share),
+                            ),
+                            Text("Share Again"),
+                          ],
+                        ),
+                        Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.share),
+                            ),
+                            Text("Share Again"),
+                          ],
+                        ),
+                        Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.share),
+                            ),
+                            Text("Share Again"),
+                          ],
+                        ),
+                        Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.share),
+                            ),
+                            Text("Share Again"),
+                          ],
+                        ),
+                        Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.share),
+                            ),
+                            Text("Share Again"),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
